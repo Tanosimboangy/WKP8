806f86224ccd2b71a85db89bec184ce983703005
Tanosimboangy <jacquit.aja@onja.org>
2020-08-22 11:38:04 +0300

    adjusting the style for the list items

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let songs = [];
 
 const form = document.querySelector('form');
-console.log(form);
 const showList = document.querySelector('.show_list');
-console.log(showList);
 
 const showSong = () => {
     const html = songs.map(song => {
@@ -12,7 +10,7 @@ const showSong = () => {
             <li>${song.picture}</li>
             <li>
                 <ul>
-                    <li><h2>Title: ${song.picture}</h2></li>
+                    <li><h2>Title: ${song.title}</h2></li>
                     <li><small>Style: ${song.style}</small></li>
                 </ul>
             </li>
@@ -22,7 +20,7 @@ const showSong = () => {
                     <li><small>Length: ${song.length}</small></li>
                 </ul>
             </li>
-            <li><h2>SCORE: ${song.score}</h2></li>
+            <li><h2>SCORE: </h2></li>
             <li><button class="add_score">+1</button></li>
             <li>
                 <button value="${song.id}" class="delete" aria-label="delete song ${song.title}">
@@ -33,15 +31,14 @@ const showSong = () => {
         `;
     }).join("");
     showList.innerHTML = html;
-    console.log(html);
 }
+// Calling the function
 showSong();
 
+// Creating a new object to store the value from the users
 const addSong = (e) => {
-    console.log(e.currentTarget);
     e.preventDefault();
     formitem = e.currentTarget;
-    console.log(e);
     const newSong = {
         title: formitem.title.value,
         name: formitem.name.value,
@@ -50,19 +47,33 @@ const addSong = (e) => {
         picture: formitem.picture.value,
         id: Date.now(),
     };
+    // Push the new object into the big array of object
     songs.push(newSong);
+    // Running the dispatcheEvent in order to reun this function
     showList.dispatchEvent(new CustomEvent('songListUpdated'));
     showSong();
+    // Reset the form
     formitem.reset();
 };
-console.log(addSong);
-
 
 // const showList = document.querySelector('.show_list');
 const filter = document.querySelector('.filter');
 
 // Handle with the add click button from the form
-const handleClick = () => {};
+const handleClick = () => {
+    const deleteBtn = e.target.matches("button.delete");
+    if (deleteBtn) {
+        const id = Number(deleteBtn.value);
+        deleteSong(id);
+        console.log(id);
+    }
+};
+
+const deleteSong = idDelete => {
+    songs = songs.filter(song => song.id !== idDelete);
+    console.log(idDelete);
+    showList.dispatchEvent(new CustomEvent('songListUpdated'));
+}
 
 // Filter the songs by its title
 const searchSongFilter = () => {};
@@ -81,8 +92,8 @@ const deletebutton = () => {};
 
 const intoLocalStorage = () => {};
 
-const updataLocalStorage = () => {};
-
+const updataLocalStorage = (e) => {};
 
 form.addEventListener('submit', addSong);
 showList.dispatchEvent(new CustomEvent('songListUpdated', showSong));
+showList.dispatchEvent(new CustomEvent('songListUpdated', handleClick));
