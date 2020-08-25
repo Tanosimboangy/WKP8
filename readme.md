9e675ebeec6e5b4e451091b5179feea1f5ca5c20
Tanosimboangy <jacquit.aja@onja.org>
2020-08-25 16:30:00 +0300

    finishing the task

--- readme.md
+++ readme.md
@@ -1,22 +1,26 @@
 # In a few sentences, explain the structure of your project. 
 1_ First of all, I did not manage to complete this project untill the end. These are the steps I followed while working on this project :
 - For starting, as usual , I crated the necessary HTML and styling it.
-- I began my work by using the fundamental approach to map through the arrays of object, followed by crating a new HTML and pushing the values from the map into that HTML, so that we can show the lists of the songs for the users. All of these steps were done inside of a function.
+- I began my work by using the fundamental approach to map through the arrays of object, followed by creating a new HTML and pushing the values from the map into that new HTML, so that we can show the lists of the songs for the users. All of these steps were done inside of a function.
 - What's more, I tried to access all the imputs elements in oreder to get the values from the users. After that, I created a new function and inside I created a new object, which contains the values from the user. I pushed the new object into the big arrays of object, and then, I created the HTML for that to show the list of the songs with the new one.
 - I could mange to store the user's imput into the local storage
+- I tried to make the score button work but I could not.
+- For the filter input search, I could not mange to fulfill it. I looked it up on the internet but I did not find any idea.
 
 # If you had more time, what area of your project would you improve?
 - I would improve the area of local storage. I did it but I still do not really understand very well how it works.
 - On top of that, I do really need to improve my understanding on what to use according to what the goal is and how to do that.That was my weakest point during this project. Probably, I need to watch the videos more often.
+- I really need to enhance the way to make a research on the intenet
 
 # Did you learn anything new while working on this project?
 - I learned how to debug by myself
 # What was the most challenging part for you?
-- It is hard for me to sort all of the lists by using the score
+- It is hard for me to sort all of the lists by the lowest score to the biggest score
 
 # Would you like a new explanation about a specific topic?
-- About filtering the list by its names 
+- About filtering the list by its title and its style
+- Also, I want to know the mehtod to activate the reset filter button
 
 # Any other comments?
-- I know it is graduated but the time was not long enogth, also I am really slow
-- Could you give me a hand to improve my way of debugging
+- I am really slow at understanding the concepts
+- Could you give me a hand to improve my way of undrestanding and debugging
--- script.js
+++ script.js
@@ -61,7 +61,6 @@ const addSong = (e) => {
 const handleClick = e => {
     const scoreValue = e.target.closest(".score");
     const addingScore = e.target.closest(".add_score");
-    scoreValue.value = +1;
     if (addingScore) {
         const id = Number(addingScore.value);
         addingScoreSong(id);
@@ -79,9 +78,6 @@ const handleClick = e => {
 const addingScoreSong = idAddSong => {
     // songs = songs.filter(song => song.id !== idAddSong);
     console.log('I have got it');
-    // if (id = idAddSong ) {
-
-    // }
 }
 
 // Filtering the items by removing the one which has the matched id 
@@ -92,48 +88,38 @@ const deleteSong = idDelete => {
     showList.dispatchEvent(new CustomEvent('songListUpdated'));
 }
 
+// Incrementing the score any time the score button is ckicked
+const score = (e) => {
+    const addingScore = e.target.closest(".add_score");
+    increment(id);
+    showList.dispatchEvent(new CustomEvent('songListUpdated'));
+    console.log(e);
+    addingScore.addEventListener("click", increment);
+};
 
-// Incrementing the score any time this is ckicked
-// const score = (e) => {
-//     const addingScore = e.target.closest(".add_score");
-//     increment(id);
-//     showList.dispatchEvent(new CustomEvent('songListUpdated'));
-//     console.log(e);
-//     addingScore.addEventListener("click", increment);
-// };
-
-
-// // Increment the number of the score
-// const increment = e => {
-//     const score = e.target.closest("score");
-//     if (score) {
-//         const value = score.value +1;
-//         console.log(value);
-//     }
-//     showList.dispatchEvent(new CustomEvent('songListUpdated'));
-//     score();
-// }
-
-
-
-
-
+// Increment the score
+const increment = e => {
+    const score = e.target.closest("score");
+    score.addEventListener("click", )
+    if (score) {
+        const value = score.value +1;
+        console.log(value);
+    }
+    showList.dispatchEvent(new CustomEvent('songListUpdated'));
+    score();
+}
 
 // Grabbing these necessary elements
 const inputFilterTitle = document.querySelector('.filter_title');
 const resetBtnFilter = document.querySelector('.reset_filter');
 
 // Filter the songs by its title
-const searchSongFilterTitle = () => {
+const searchSongFilterTitle = e => {
     const filterdata = songs.filter(song => song.title.toLowercase().includes(inputFilterTitle.toLowercase()));
     showSong(filterdata);
     showList.dispatchEvent(new CustomEvent('songListUpdated'));
     console.log("I have got to go");
 };
-
-
-
-
 
 // Filter the songs by using its style
 const searchSongFilterStyle = () => {};
@@ -141,7 +127,6 @@ const searchSongFilterStyle = () => {};
 // Reset the fitler after filter the songs
 const resetFilter = () => {
     console.log("I do not like being called with that name");
-    // showList.dispatchEvent(new CustomEvent('songListUpdated'));
     showSong();
 };
 resetBtnFilter.addEventListener('click', resetFilter);
